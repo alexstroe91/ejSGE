e3a38a79e189753f381739c0992736305ee998d7
alexstroe91 <alexstroe91@gmail.com>
2021-11-15 09:22:46 +0100

    Terminado el cifrado cesar

---
+++ 5.-for/for2.py
@@ -0,0 +1,31 @@
+from typing import FrozenSet
+
+
+letra = (input("Introduce una letra: "))
+
+numLineas = int(input("Introduce un numero de lineas: "))
+
+cadena = ""
+
+for i in range(numLineas):
+    for j in range(numLineas * 2):
+        if j >= numLineas - i and j <= numLineas + i:
+            cadena += letra
+        else:
+            cadena += " "
+    print(cadena)
+    cadena = ""
+
+frase = str(input("Introduce la frase: "))
+desplazamiento = int(input("Introduce el desplazamiento: "))
+resultado = ""
+
+for j in range(len(frase)):
+
+    if (ord(frase[j]) + desplazamiento) > 122:
+        resultado += str.lower(chr(ord(frase[j]) + desplazamiento - 26) )
+    else:
+        resultado += str.lower(chr(ord(frase[j]) + desplazamiento))
+        
+
+print(resultado)
