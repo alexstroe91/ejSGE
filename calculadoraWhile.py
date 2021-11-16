e580d0a4eb32cb8b34f17d5a52ce9c7a62139027
alexstroe91 <alexstroe91@gmail.com>
2021-11-16 11:00:20 +0100

    Pasado el menu por funcion

--- calculadoraWhile.py
+++ calculadoraWhile.py
@@ -8,17 +8,23 @@ def multiplicar(op1, op2):
     print("{0} * {1} = {2}".format(op1, op2, op1 * op2))
 
 def dividir(op1, op2):
-    print("{0} / {1} = {2}".format(op1, op2, op1 / op2))
+    if op2 == 0:
+        print("El dividendo no puede ser 0")
+    else:
+        print("{0} / {1} = {2}".format(op1, op2, op1 / op2))
 
+def menu():
+    print("1-sumar")
+    print("2-restar")
+    print("3-multiplicar")
+    print("4-dividir")
 
 respuesta = "s"
 
 while respuesta != "n":
 
-    print("1-sumar")
-    print("2-restar")
-    print("3-multiplicar")
-    print("4-dividir")
+    menu()
+    
     opcion = int(input("Elige una opcion: "))
 
     num1 = int(input("Introduce el primer numero: "))
