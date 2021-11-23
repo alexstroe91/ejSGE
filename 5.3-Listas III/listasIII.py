a385efeeb8077505fe166e2e1f25eabccbc9be6a
alexstroe91 <alexstroe91@gmail.com>
2021-11-23 10:28:58 +0100

    Creado el la funcion del menu y llamado

---
+++ 5.3-Listas III/listasIII.py
@@ -0,0 +1,12 @@
+def menu ():
+    print("1.- Añadir usuario")
+    print("2.- Mostrar todos los usuarios")
+    print("3.- Modificar usuarios dado un ID")
+    print("4.- Borrar usuario dado un ID")
+    print("5.- Mostrar solo las fechas")
+
+continuar = "s"
+
+while continuar == "s":
+    menu()
+    
