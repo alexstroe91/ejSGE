4fe15f0bde7ad28aa02437d63607e8a858adeee7
alexstroe91 <alexstroe91@gmail.com>
2021-11-23 10:42:33 +0100

    Creado metodo de añadir usuario y llamado en el menu

--- 5.3-Listas III/listasIII.py
+++ 5.3-Listas III/listasIII.py
@@ -1,12 +1,31 @@
+continuar = "s"
+listaUsuarios = []
+id = 1
+
 def menu ():
     print("1.- Añadir usuario")
     print("2.- Mostrar todos los usuarios")
     print("3.- Modificar usuarios dado un ID")
     print("4.- Borrar usuario dado un ID")
     print("5.- Mostrar solo las fechas")
+    
+def añadirUser(id):
+    diaFecha = int(input("Introduce el dia: "))
+    mesFecha = int(input("Introduce el mes: "))
+    añoFecha = int(input("Introduce el año: "))
+        
+    rol = input("Rol del usuario: [usuario, usuario avanzado, admin]: ")
+        
+    listaUsuarios.append([id, [diaFecha, mesFecha, añoFecha], rol])
+    
 
-continuar = "s"
 
 while continuar == "s":
     menu()
+    opcion = int(input("Elige una opcion: "))
+    if opcion == 1:
+        añadirUser(id)
+        id += 1
+    
+print(listaUsuarios)
     
