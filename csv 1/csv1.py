67784a66040e53d6bd9d2722bcb7a4fe44dc59d2
alexstroe91 <alexstroe91@gmail.com>
2022-01-18 10:52:08 +0100

    Creados los resultados de la liga

---
+++ csv 1/csv1.py
@@ -0,0 +1,14 @@
+datos = []
+
+datos.append(["Local", "Visitante", "Resultado", "Tarjetas Amarillas", "Tarjetas Rojas"])
+datos.append(["Fuenlabrada", "Almeria", "1-0", 3,  0])
+datos.append(["Valladolid", "Eibar", "1-1", 0,  0])
+datos.append(["Oviedo", "Cartagena", "0-0", 2,  1])
+datos.append(["Girona", "Almeria", "0-3", 3,  0])
+datos.append(["Burgos", "Cartagena", "0-0", 1,  0])
+datos.append(["Amorrebieta", "Lugo", "3-3", 2,  0])
+datos.append(["Zaragoza", "Lugo", "1-3", 2,  0])
+datos.append(["Malaga", "Fuenlabrada", "3-3", 2,  1])
+datos.append(["Ibiza", "Ponferradina", "2-2", 3,  0])
+datos.append(["Tenerife", "Huesca", "1-2", 4,  1])
+datos.append(["Leganes", "Mirandes", "1-1", 2,  0])
