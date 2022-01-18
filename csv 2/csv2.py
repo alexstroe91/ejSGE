60098104d561e666c69d803eac29a9bb4deb38dc
alexstroe91 <alexstroe91@gmail.com>
2022-01-18 17:38:59 +0100

    Contado los goles, las tarjetas, los espectadores y la media de los mismos

--- csv 2/csv2.py
+++ csv 2/csv2.py
@@ -10,8 +10,21 @@ with open("liga.csv") as fich:
     
 partidos.pop(0)
     
+totalTarjetas = 0
+totalEspectadores = 0
+totalPartidos = 0
+    
 for partido in partidos:
     resultados.append(partido[2].split("-"))
+    
+    totalTarjetas += int(partido[3])
+    totalTarjetas += int(partido[4])
+    
+    totalEspectadores += int(partido[5])
+    
+    totalPartidos += 1
+    
+mediaEspectadores = totalEspectadores / totalPartidos
     
 victoriasLocal = 0
 victoriasVisitante = 0
@@ -25,9 +38,10 @@ for resultado in resultados:
     
     totalGoles += int(resultado[0])
     totalGoles += int(resultado[1])
+    
 
-print("Hay " ,(victoriasLocal) , " victorias para los locales")
-print("Hay " , (victoriasVisitante), " victorias para los visitantes")
+print("Hay" ,(victoriasLocal) , "victorias para los locales")
+print("Hay" , (victoriasVisitante), "victorias para los visitantes")
 
 if victoriasLocal > victoriasVisitante:
     print("Han ganado mas los equipos locales")
@@ -36,4 +50,10 @@ elif victoriasLocal < victoriasVisitante:
 else:
     print("Han ganado los mismos partidos")
     
-print("Se han marcado un total de: ",totalGoles)
+print("Se han marcado un total de",totalGoles, "goles")
+
+print("Se han sacado un total de", totalTarjetas, "tarjetas")
+
+print("Los espectadores totales han sido", round(totalEspectadores, 2))
+
+print("La media de los espectadores ha sido de", round(mediaEspectadores, 2))
