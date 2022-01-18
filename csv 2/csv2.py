307b40adff4083956c5a6cd83ff5655e1a08cd2f
alexstroe91 <alexstroe91@gmail.com>
2022-01-18 17:32:02 +0100

    Mostrado mensaje de cuantos partidos han ganado locales y visitantes Mostrado el total de goles marcados

--- csv 2/csv2.py
+++ csv 2/csv2.py
@@ -15,12 +15,19 @@ for partido in partidos:
     
 victoriasLocal = 0
 victoriasVisitante = 0
+totalGoles = 0
 
 for resultado in resultados:
     if resultado[0] > resultado[1]:
         victoriasLocal += 1
     elif resultado[0] < resultado[1]:
         victoriasVisitante += 1
+    
+    totalGoles += int(resultado[0])
+    totalGoles += int(resultado[1])
+
+print("Hay " ,(victoriasLocal) , " victorias para los locales")
+print("Hay " , (victoriasVisitante), " victorias para los visitantes")
 
 if victoriasLocal > victoriasVisitante:
     print("Han ganado mas los equipos locales")
@@ -28,3 +35,5 @@ elif victoriasLocal < victoriasVisitante:
     print("Han ganado mas los equipos visitantes")
 else:
     print("Han ganado los mismos partidos")
+    
+print("Se han marcado un total de: ",totalGoles)
