0846494d0b6e56a8d7beae48b7ab85456e6480c8
alexstroe91 <alexstroe91@gmail.com>
2022-01-22 12:45:54 +0100

    Creado el bucle

--- parking/parking.py
+++ parking/parking.py
@@ -3,25 +3,40 @@ coche = []
 
 def menu():
     print("1.- Crear ticket")
+    print("2.- Cerrar ticket")
+    print("3.- Salir")
     
     opcion = int(input("Elige a opcion: "))
     
     return opcion
     
 def crearTicket():
+    coche = []
     matricula = input("Matricula: ")
-    horaEntrada = input("Hora entrada: ")
+    horaEntrada = input("Hora y minuto ENTRADA: ")
     estado = "Dentro"
     coche.append(matricula)
     coche.append(horaEntrada)
     coche.append(estado)
     
     listaCoches.append(coche)
+    
+def cerrarTicket():
+    horaSalida = input("Hora y minuto SALIDA: ")
 
-if menu() == 1:
-    crearTicket()
-    
-    
+opcion = 0
+
+while opcion != 3:
+    opcion = menu()
+
+    if opcion == 1:
+        crearTicket()
+    elif opcion == 2:
+        print("")
+    elif opcion == 3:
+        print("                 -------- Has salido --------")
+        
+        
 
 print(coche)
 print(listaCoches)
