9dbf19beac04d0be89d59da5106d65173bdc0a75
alexstroe91 <alexstroe91@gmail.com>
2022-02-14 10:55:15 +0100

    creado los dos ficheros y empezada la funcion

---
+++ exception/fichero1.py
@@ -0,0 +1,6 @@
+#aqui la funcion que comprueba el tamaño de las listas
+
+def funcExcepcion(lista1, lista2):
+    
+    if len(lista1) > len(lista2):
+        #lanzar excepcion
---
+++ exception/fichero2.py
@@ -0,0 +1,6 @@
+import fichero1 as fic
+
+#aqui las listas y la llamada a la funcion del otro fichero
+
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
