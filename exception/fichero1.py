89c4be5cc356c5c4c09e240722c067de6e6da0c7
alexstroe91 <alexstroe91@gmail.com>
2022-02-14 15:38:48 +0100

    Creado la funcion y llamada en el otro fichero

--- exception/fichero1.py
+++ exception/fichero1.py
@@ -1,6 +1,18 @@
-#aqui la funcion que comprueba el tamaño de las listas
 
+#definimos la funcion
 def funcExcepcion(lista1, lista2):
     
+    #comprobamos que lista es mayor y lanzamos la excepcion correspondiente
     if len(lista1) > len(lista2):
-        #lanzar excepcion
+        raise Exception("La lista 1 es mayor que la lista 2.")
+    elif len(lista1) < len(lista2):
+        raise Exception("La lista 1 es mayor que la lista 2.")
+    else:
+        #si son iguales de tamaño comprobamos que el divisor no sea 0 y si no es, se hace que
+        #la division
+        for i in range(len(lista1)):
+            if lista2[i] == 0:
+                raise ZeroDivisionError("No se puede dividir por 0.")
+            else:
+                print("{0} / {1} = {2}".format(lista1[i], lista2[i], lista1[i]/lista2[i]))
+        
