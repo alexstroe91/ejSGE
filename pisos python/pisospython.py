615c4682d67655a93c7202b05ef08b8353c6d1c5
alexstroe91 <alexstroe91@gmail.com>
2022-02-21 09:56:09 +0100

    Hago replace de Pisos a Pisosnuevos en el create table y en los insert y despues conecto con la BD y ejecuto las querys

--- pisos python/pisospython.py
+++ pisos python/pisospython.py
@@ -19,7 +19,46 @@ with open("tabla-pisos.sql", "r", encoding="utf-8") as fic:
         sqlCreateTable.append(texto)
         texto = fic.read()
         
-for i in sqlCreateTable:
-    print(i)
-    
+        
+textoNuevo = sqlinsert[0]
+textoNuevo = textoNuevo.replace("pisos", "pisosNuevos")
+
+sqlinsert.clear()
+sqlinsert.insert(0, textoNuevo)   
+     
+textoNuevo = ""
+
+textoNuevo = sqlCreateTable[0]
+textoNuevo = textoNuevo.replace("pisos", "pisosNuevos")
+
+sqlCreateTable.clear()
+sqlCreateTable.insert(0, textoNuevo)
+        
+        
+try:
+    #creamos conexión
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database="pruebadb",
+        user="pruebas",
+        password="prueba123")
+    #creamos un cursor
+    cur = conn.cursor()
+    #ejecutamos la query
+        
+    for i in sqlinsert:
+        cur.execute(i)
+        
+    #extraemos la primera linea
+    #print(cur.fetchone())
+    #cerramos el cursor
+    conn.commit()
+    cur.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        cur.close()
+        conn.close()
+        print('Database connection closed.')        
     
